811ab38731d5a14c4f652b7e7e181953d792aa5f
DESKTOP-EKEURC0\anhei <anhei19960725@yahoo.co.jp>
2021-07-06 16:14:06 +0900

    shownameじっそｙ

--- HelloWorld_An/HelloWorld_An/main.cpp
+++ HelloWorld_An/HelloWorld_An/main.cpp
@@ -1,10 +1,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include "kinoshita.h"
 
 int main()
 {
 	std::cout << "HelloWorld\n";
+
+	kinoshita my;
+	my.ShowName();
 
 	system("pause");
 	return 0;
