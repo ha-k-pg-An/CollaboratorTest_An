a36d9bc1d840346b201cd9818b5d1fbcb865a555
DESKTOP-EKEURC0\anhei <anhei19960725@yahoo.co.jp>
2021-07-06 16:38:59 +0900

    myan.showname();追加

--- HelloWorld_kinoshita/HelloWorld_kinoshita/main.cpp
+++ HelloWorld_kinoshita/HelloWorld_kinoshita/main.cpp
@@ -1,12 +1,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "kinoshita.h"
+#include "An.h"
 
 int main()
 {
 	printf("HelloWorld\n");
 	kinoshita my;
 	my.ShowName();
+
+	An myan;
+	myan.ShowName();
+
 	system("pause");
 		
 	return 0;
