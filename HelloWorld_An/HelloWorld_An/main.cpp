47a9d19f1d2223707319103f7b79fd1af313576d
DESKTOP-EKEURC0\anhei <anhei19960725@yahoo.co.jp>
2021-07-06 17:11:38 +0900

    an class のshowname関数の更新

--- HelloWorld_An/HelloWorld_An/main.cpp
+++ HelloWorld_An/HelloWorld_An/main.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <iostream>
+#include "An.h"
 
 int main()
 {
 	std::cout << "HelloWorld\n";
+
+	An my;
+	my.ShowName();
+
 
 	system("pause");
 	return 0;
